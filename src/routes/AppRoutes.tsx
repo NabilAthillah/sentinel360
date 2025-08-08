d304ddeca70200041aa5edebf0a8ad9398160670
Nabil <133586196+NabilAthillah@users.noreply.github.com>
2025-08-08 23:49:34 +0700

    update

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,59 +1,62 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import AttendancePage from '../pages/attendance/AttendancePage';
+import AuditTrails from '../pages/auditTrails/AuditTrails';
 import LoginPage from '../pages/auth/LoginPage';
 import DashboardPage from '../pages/dashboard/DashboardPage';
 import EmployeesPage from '../pages/employees/EmployeesPage';
-import AttendancePage from '../pages/attendance/AttendancePage';
-import OccurencePage from '../pages/occurence/OccurencePage';
+import IncidentTypePage from '../pages/incident/IncidentPage';
+import IncidentPage from '../pages/incidentType/IncidentType';
+import HistoryPage from '../pages/learning/HistoryPage';
 import LearningPage from '../pages/learning/LearningPage';
 import LeaveManagementPage from '../pages/leaveManagement/LeaveManagementPage';
+import OccurencePage from '../pages/occurence/OccurencePage';
+import PreEmployeePage from '../pages/preEmployee/PreEmployeePage';
 import SettingsAttendancePage from '../pages/setings/attendance/SettingsAttendancePage';
 import ClientInfoPage from '../pages/setings/client/ClientInfoPage';
 import EmployeeDocumentPage from '../pages/setings/employeeDocument/EmployeeDocumentPage';
+import IncidentPageMaster from '../pages/setings/incident/IncidentPage';
 import OccurrenceCatgPage from '../pages/setings/occurrenceCatg/OccurrenceCatgPage';
+import ProfilePage from '../pages/setings/profile/ProfilePage';
 import RolesPage from '../pages/setings/roles/RolesPage';
 import SopDocumentPage from '../pages/setings/sop-document/SopDocumentPage';
-import ProfilePage from '../pages/setings/profile/ProfilePage';
-import HistoryPage from '../pages/learning/HistoryPage';
+import AllocationPage from '../pages/site/AllocationPage';
+import HomePage from '../pages/user/home/HomePage';
+import MapPage from '../pages/site/MapPage';
 import SitePage from '../pages/site/SitePage';
 import RoutePage from '../pages/site/RoutePage';
-import MapPage from '../pages/site/MapPage';
-import AllocationPage from '../pages/site/AllocationPage';
-import IncidentPageMaster from '../pages/setings/incident/IncidentPage';
-import IncidentTypePage from '../pages/incident/IncidentPage';
-import IncidentPage from '../pages/incidentType/IncidentType';
-import PreEmployeePage from '../pages/preEmployee/PreEmployeePage';
-import AuditTrails from '../pages/auditTrails/AuditTrails';
 
 const AppRoutes = () => {
   return (
     <Router>
-        <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path='/login' element={<LoginPage />}/>
-            <Route path='/dashboard' element={<DashboardPage />}/>
-            <Route path='/employees' element={<EmployeesPage />}/>
-            <Route path='/attendance' element={<AttendancePage />}/>
-            <Route path='/e-occurrence' element={<OccurencePage />}/>
-            <Route path='/e-learning' element={<LearningPage />}/>
-            <Route path='/e-learning/history' element={<HistoryPage />}/>
-            <Route path='/incidents' element={< IncidentTypePage/>}/>
-            <Route path='/leave-management' element={<LeaveManagementPage />}/>
-            <Route path='/sites' element={<SitePage />}/>
-            <Route path='/sites/map' element={<MapPage />}/>
-            <Route path='/sites/allocation' element={<AllocationPage />}/>
-            <Route path='/sites/:idSite/routes' element={<RoutePage />}/>
-            <Route path='/settings/attendance' element={<SettingsAttendancePage />}/>
-            <Route path='/settings/client-info' element={<ClientInfoPage />}/>
-            <Route path='/settings/employee-document' element={<EmployeeDocumentPage />}/>
-            <Route path='/settings/incident' element={<IncidentPageMaster />}/>
-            <Route path='/settings/occurrence-catg' element={<OccurrenceCatgPage />}/>
-            <Route path='/settings/roles' element={<RolesPage />}/>
-            <Route path='/settings/sop-document' element={<SopDocumentPage />}/>
-            <Route path='/settings/profile' element={<ProfilePage />}/>
-            <Route path='/incidentfree' element={< IncidentPage />}/>
-            <Route path='/pre-employees' element={< PreEmployeePage />}/>
-            <Route path='/audit-trails' element={< AuditTrails />}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/employees' element={<EmployeesPage />} />
+        <Route path='/attendance' element={<AttendancePage />} />
+        <Route path='/e-occurrence' element={<OccurencePage />} />
+        <Route path='/e-learning' element={<LearningPage />} />
+        <Route path='/e-learning/history' element={<HistoryPage />} />
+        <Route path='/incidents' element={< IncidentTypePage />} />
+        <Route path='/leave-management' element={<LeaveManagementPage />} />
+        <Route path='/sites' element={<SitePage />} />
+        <Route path='/sites/map' element={<MapPage />} />
+        <Route path='/sites/allocation' element={<AllocationPage />} />
+        <Route path='/sites/:idSite/routes' element={<RoutePage />} />
+        <Route path='/settings/attendance' element={<SettingsAttendancePage />} />
+        <Route path='/settings/client-info' element={<ClientInfoPage />} />
+        <Route path='/settings/employee-document' element={<EmployeeDocumentPage />} />
+        <Route path='/settings/incident' element={<IncidentPageMaster />} />
+        <Route path='/settings/occurrence-catg' element={<OccurrenceCatgPage />} />
+        <Route path='/settings/roles' element={<RolesPage />} />
+        <Route path='/settings/sop-document' element={<SopDocumentPage />} />
+        <Route path='/settings/profile' element={<ProfilePage />} />
+        <Route path='/incidentfree' element={< IncidentPage />} />
+        <Route path='/pre-employees' element={< PreEmployeePage />} />
+        <Route path='/audit-trails' element={< AuditTrails />} />
+
+        <Route path='/user' element={<HomePage />} />
+      </Routes>
     </Router>
   )
 }
